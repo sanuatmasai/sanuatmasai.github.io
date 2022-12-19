d15b713b52905a27b03790e90f3ba6efb314ca68
sanuatmasai <sanuatmasai@gmail.com>
2022-12-19 18:20:28 +0530

    portfolio modified

--- script.js
+++ script.js
@@ -68,27 +68,112 @@ $(document).ready(function(){
     //         autoplayHoverPause: true
     //     });
     // });
-    $('.carousel').owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplay: true,
-        slideTransition: 'linear',
-        autoplaySpeed: 20000,
-        autoplayTimeOut: 1000,
-        autoplayHoverPause: false,
-        responsive: {
-            0:{
-                items: 1,
-                nav: false
-            },
-            600:{
-                items: 2,
-                nav: false
-            },
-            1000:{
-                items: 3,
-                nav: false
-            }
-        }
-    });
+
+
+
+
+
+    // $('.carousel').owlCarousel({
+        // margin: 20,
+        // autoplay: true,
+        // autoplayHoverPause: false,
+        // responsive: {
+        //     0:{
+        //         items: 1,
+        //         nav: false
+        //     },
+        //     600:{
+        //         items: 2,
+        //         nav: false
+        //     },
+        //     1000:{
+        //         items: 3,
+        //         nav: false
+        //     }
+        // }
+    // });
+
+
+    window.addEventListener('scroll',function(){
+        reveal('.right');
+        reveal('.left')
+      });
+      
+      function reveal(x){
+          let reveals = document.querySelectorAll(x);
+      
+          for (let i = 0; i < reveals.length; i++) {
+      
+              let windowwidth = window.innerWidth;
+              let revealtop = reveals[i].getBoundingClientRect().top;
+              let revealpoint = 600;
+              
+              if(revealtop < windowwidth - revealpoint){
+                  reveals[i].classList.add('active');
+              }else{
+                  reveals[i].classList.remove('active');
+              }
+          }
+      }
+      
+      window.addEventListener('scroll',reveal3);
+      
+      function reveal3(){
+          let reveals = document.querySelectorAll('.down');
+      
+          for (let i = 0; i < reveals.length; i++) {
+      
+              let windowheight = window.innerHeight;
+              let revealtop = reveals[i].getBoundingClientRect().top;
+              let revealpoint = 350;
+              
+              if(revealtop < windowheight - revealpoint){
+                  reveals[i].classList.add('scrolldown');
+              }else{
+                  reveals[i].classList.remove('scrolldown');
+              }
+          }
+      }
+      
+      
+      window.addEventListener('scroll',reveal2);
+      
+      function reveal2(){
+          let reveals = document.querySelectorAll('.slideup');
+      
+          for (let i = 0; i < reveals.length; i++) {
+      
+              let windowheight = window.innerHeight;
+              let revealtop = reveals[i].getBoundingClientRect().top;
+              let revealpoint = 160;
+              
+              if(revealtop < windowheight - revealpoint){
+                  reveals[i].classList.add('scrollup');
+              }else{
+                  reveals[i].classList.remove('scrollup');
+              }
+          }
+      }
+      
+      
+      window.addEventListener('scroll',()=>{
+        let x = document.querySelector('.iconsticky');
+        x.classList.toggle('arrow',window.scrollY > 500);
+      })
+      
+      
+      // mediaquery navbar first size javacript ///
+      
+      const ham = document.querySelector(".ham");
+      const title = document.querySelector(".title");
+      
+      ham.addEventListener("click", () => {
+        ham.classList.toggle("active");
+        title.classList.toggle("active");
+      } )
+      
+      document.querySelectorAll(".remove-active").forEach(n => n.addEventListener("click", () => {
+        ham.classList.remove("active");
+        title.classList.remove("active");
+      }));
 });
